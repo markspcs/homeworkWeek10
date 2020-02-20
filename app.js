ec4470d9cdc78c8989c0b962c45d7ce8ae06d859
Mark Stout <markspcs@yahoo.com>
2020-02-19 19:25:52 -0600

    update to gotten information

--- app.js
+++ app.js
@@ -2,7 +2,47 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const inquirer = require("inquirer");
 
-const employee = new Engineer('mark', 1, 'markspcs@yahoo.com', 'markspcs' );
+const debug = 1;
+const employee = new Engineer('mark', 1, 'markspcs@yahoo.com', 'markspcs');
 
 console.log(employee);
+askUser();
+//////////////////////////////////////////////////////////////
+//async to prompt user
+async function askUser() {
+    let addMore = "y";
+    do {
+        const response = await promptUser();
+        addMore = response.end;
+        debug && console.log(response);
+    } while (addMore === "y");
+
+}
+///////////////////////////////////////////////////////////////////
+//prompt user for input and return username location
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: 'list',
+            message: 'Position:',
+            name: 'position',
+            choices: [
+              'Manager',
+              'Engineer',
+              'Intern',
+            ],
+          },
+        {
+            type: "input",
+            name: "end",
+            message: "add another user [y/n]?"
+        }
+    ]);
+}
